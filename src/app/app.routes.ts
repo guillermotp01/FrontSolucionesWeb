a7c8e5667d91f9c63a96f5a4ebcec57e5e109655
Theylor Gadea Rios <129561609+TheylorGR@users.noreply.github.com>
2024-06-01 04:38:45 -0500

    funcion boton sacar cita, registrarcita front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import PrincipalComponent from './components/principal/principal.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { CitaComponent } from './components/cita/cita.component';
+import { MIcitaComponent } from './components/micita/micita.component';
 
 export const routes: Routes = [
     { 'path': '', component:PrincipalComponent },
@@ -10,4 +11,5 @@ export const routes: Routes = [
     { 'path': 'nosotros', component:NosotrosComponent },
     { 'path': 'contacto', component:ContactoComponent },
     { 'path': 'cita', component:CitaComponent },
+    { 'path': 'micita', component:MIcitaComponent}
 ];
--- src/app/components/cita/cita.component.ts
+++ src/app/components/cita/cita.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CitaService } from '../../Services/cita.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Cita } from '../../Models/Cita';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cita',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cita.component.html',
-  styleUrl: './cita.component.css'
+  styleUrls: ['./cita.component.css']
 })
-export class CitaComponent {
+export class CitaComponent implements OnInit {
+  cita: Cita;
 
+  constructor(private citaService: CitaService, private router: Router) {
+    this.cita = this.initializeCita();
+  }
+
+  ngOnInit(): void {}
+
+  initializeCita(): Cita {
+    return {
+      fechacita: new Date(),
+      horacita: { hours: 0, minutes: 0 },
+      nombre: '',
+      apellido: '',
+      telefono: 0,
+      direccion: '',
+      fecha_nac: new Date(),
+      correo: '',
+      sexo: '',
+      tipodoc: 0,
+      num_doc: 0,
+      marca: '',
+      modelo: '',
+      año: new Date(),
+      color: '',
+      motor: '',
+      placa: '',
+      obs: ''
+    };
+  }
+
+  registrarCita(): void {
+    this.citaService.registrarCita(this.cita).subscribe(
+      response => {
+        console.log('Cita registrada exitosamente', response);
+        this.regreso();
+        this.cita = this.initializeCita();
+        alert("La cita se registro correctamente"); 
+      },
+      error => {
+        console.error('Error al registrar la cita', error);
+        alert("La cita no se pudo registrar correctamente");
+      }
+    );
+  }
+
+  regreso(){
+    this.router.navigate(['/cita']); 
+  }
 }
+
