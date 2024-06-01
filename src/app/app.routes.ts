0d02fdac91d842eca75211c7f6780764d2e924b6
Marfalling <dame.tufuerza.pegazo988@gmail.com>
2024-06-01 00:58:40 -0500

    CAMBIOS CITA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import PrincipalComponent from './components/principal/principal.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { CitaComponent } from './components/cita/cita.component';
 
 export const routes: Routes = [
     { 'path': '', component:PrincipalComponent },
     { 'path': 'inicio', component:PrincipalComponent },
     { 'path': 'nosotros', component:NosotrosComponent },
     { 'path': 'contacto', component:ContactoComponent },
+    { 'path': 'cita', component:CitaComponent },
 ];
